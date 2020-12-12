58f45d862f4af7908dbae599807a7e4aac8f3600
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-12-12 19:19:19 +0900

    fix font & list style

--- src/About.jsx
+++ src/About.jsx
@@ -27,33 +27,50 @@ const StyledAbout = styled.main`
     padding: 20px;
     color: #383838;
     h3 {
-      font: normal 600 30px/34px 'Roboto';
+      font: normal 500 30px/34px 'Roboto';
       letter-spacing: 5px;
       margin: 50px 0px 50px 0px;
       color: #383838;
     }
     h4 {
-      font: normal 600 28px/32px 'Roboto';
+      font: normal 500 28px/32px 'Roboto';
       color: #383838;
     }
     h5 {
-      font: normal 600 28px/32px 'Roboto';
+      font: normal 500 28px/32px 'Roboto';
       color: #383838;
     }
     h6 {
-      font: normal 600 25px/32px 'Roboto';
+      font: normal 500 25px/32px 'Roboto';
       margin: 20px 0px 50px 0px;
     }
     p {
       color: #383838;
-      font: normal 600 23px/25px 'Roboto';
+      font: normal 500 23px/25px 'Roboto';
       margin-bottom: 20px;
     }
     ul {
       font: normal 400 23px/50px 'Roboto';
       margin-top: 30px;
       li {
+        font: normal 400 19px/50px 'Roboto';
         line-height: 32px;
+      }
+      li li {
+        /* font-size: 18px; */
+        list-style: circle;
+        text-align: justify;
+        margin-left: 30px;
+      }
+    }
+  }
+
+  @media (max-width: 960px) {
+    .textWrap {
+      ul li li {
+        list-style: circle;
+        text-align: left;
+        margin-left: 30px;
       }
     }
   }
@@ -79,6 +96,15 @@ const StyledAbout = styled.main`
           font-size: 5vw;
           letter-spacing: 2px;
         }
+        ul {
+          li {
+          }
+          li li {
+            list-style: circle;
+            text-align: left;
+            margin-left: 30px;
+          }
+        }
       }
     }
   }
@@ -91,7 +117,7 @@ function About() {
 
       <main>
         <div className='imageWrap'>
-          <img src='/img/about.jpeg' alt='조상작가 이미지' />
+          <img src='/img/about.jpeg' alt='Anyongse img' />
         </div>
 
         <div className='textWrap'>
@@ -108,7 +134,7 @@ function About() {
           </p>
           <p>​</p>
           <ul>
-            <li className='solo'>
+            <li className='expertise'>
               <ul>
                 Areas of Expertise:
                 <li>시민 연극 (Applied Theatre)</li>
@@ -130,20 +156,20 @@ function About() {
             <li className='group'>
               <ul>
                 Current Projects:
-                <li>• 2020 아동.청소년극 ‘오늘도 바람’, 아이들극장 공연 배우</li>
-                <li>• 2020 아동.청소년극 ‘보이야르의 노래’, 아이들극장 공연 조연출</li>
-                <li>• 2020 ITAC5(International Teaching Artist Conference) ‘만질 수 있는 이야기들’ 워크숍 발제</li>
-                <li>• 2020 한국문화예술교육진흥원 베트남 문화예술교육 ODA 사업 운영</li>
-                <li>• 2020 T.I.E(Theatre in Education) 관객참여연극 ‘레머디’, 노을소극장 공연 배우</li>
-                <li>• 2020 동작문화재단 ‘Me, 예술을 품다’ 동작아트갤러리 워크숍 진행</li>
-                <li>• 2020 안산 청소년열정공간99°C ‘만질 수 있는 이야기’ 워크숍 진행</li>
-                <li>• 2019 서울문화재단 ‘비밀의 숲, 사라진 아티’, 동작구 서달산 워크숍 진행</li>
-                <li>• 2019 백남준아트센터 ‘Global Groove Movement Language’ 워크숍 진행</li>
-                <li>• 2019 안산 청소년열정공간99°C ‘생명의 기억’ 워크숍 진행</li>
-                <li>• 2018 한국문화예술교육진흥원 베트남 문화예술교육 ODA 사업 운영</li>
-                <li>• 2018 T.I.E(Theatre in Education) 관객참여연극 ‘선택’, 서울방송고등학교 공연 배우</li>
-                <li>• 2017 한국문화예술교육진흥원 문화예술교육 해외탐방 A-Round 사업 진행</li>
-                <li>• 2017 한국문화예술교육진흥원 세계문화예술교육주간 ‘곳간 채우기 프로젝트’ 워크숍 진행</li>
+                <li> 2020 아동.청소년극 ‘오늘도 바람’, 아이들극장 공연 배우</li>
+                <li> 2020 아동.청소년극 ‘보이야르의 노래’, 아이들극장 공연 조연출</li>
+                <li> 2020 ITAC5(International Teaching Artist Conference) ‘만질 수 있는 이야기들’ 워크숍 발제</li>
+                <li> 2020 한국문화예술교육진흥원 베트남 문화예술교육 ODA 사업 운영</li>
+                <li> 2020 T.I.E(Theatre in Education) 관객참여연극 ‘레머디’, 노을소극장 공연 배우</li>
+                <li> 2020 동작문화재단 ‘Me, 예술을 품다’ 동작아트갤러리 워크숍 진행</li>
+                <li> 2020 안산 청소년열정공간99°C ‘만질 수 있는 이야기’ 워크숍 진행</li>
+                <li> 2019 서울문화재단 ‘비밀의 숲, 사라진 아티’, 동작구 서달산 워크숍 진행</li>
+                <li> 2019 백남준아트센터 ‘Global Groove Movement Language’ 워크숍 진행</li>
+                <li> 2019 안산 청소년열정공간99°C ‘생명의 기억’ 워크숍 진행</li>
+                <li> 2018 한국문화예술교육진흥원 베트남 문화예술교육 ODA 사업 운영</li>
+                <li> 2018 T.I.E(Theatre in Education) 관객참여연극 ‘선택’, 서울방송고등학교 공연 배우</li>
+                <li> 2017 한국문화예술교육진흥원 문화예술교육 해외탐방 A-Round 사업 진행</li>
+                <li> 2017 한국문화예술교육진흥원 세계문화예술교육주간 ‘곳간 채우기 프로젝트’ 워크숍 진행</li>
               </ul>
             </li>
           </ul>
