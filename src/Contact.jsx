7e0738e0d28647857da5a2963e1d0b7a007b2c4b
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-12-21 20:05:58 +0900

    fix style img style

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,24 +2,44 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledContact = styled.main`
-  height: 65vh;
   h2 {
     font-size: 0px;
   }
   .container {
     border-top: 1px solid lightgray;
     border-bottom: 1px solid lightgray;
-    width: 100%;
+    width: 90%;
     display: flex;
     justify-content: center;
     align-items: center;
     margin-top: 30px;
     padding: 10px 0px;
-    .contactImg {
-      display: flex;
-      width: 40%;
-      img {
-        width: 100%;
+    .thumbnail-wrappper {
+      width: 50%;
+      .thumbnail {
+        position: relative;
+        padding-top: 100%; /* 1:1 ratio */
+        overflow: hidden;
+        .centered {
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          -webkit-transform: translate(50%, 50%);
+          -ms-transform: translate(50%, 50%);
+          transform: translate(50%, 50%);
+          img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            max-width: 100%;
+            height: auto;
+            -webkit-transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+          }
+        }
       }
     }
 
@@ -56,8 +76,12 @@ function Contact() {
       <h2 className='title'>Contact</h2>
 
       <div className='container'>
-        <div className='contactImg'>
-          <img src='img/contect.jpeg' alt='컨택트 이미지'></img>
+        <div className='thumbnail-wrappper'>
+          <div className='thumbnail'>
+            <div className='centered'>
+              <img src='img/contect.jpeg' alt='컨택트 이미지'></img>
+            </div>
+          </div>
         </div>
         <div className='myform'>
           <h4>AN.YONGSE</h4>
